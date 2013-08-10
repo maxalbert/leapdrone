a89ab477380feeacad3671ee321d29ac3327197b
Maximilian Albert <maximilian.albert@gmail.com>
2013-08-10 15:30:23 +0100

    Replace a few keys with ones that are actually recognised.

--- test_drone/control.js
+++ test_drone/control.js
@@ -6,22 +6,24 @@ var app = require('express')()
 var server = require('http').createServer(app)
 var io = require('socket.io').listen(server);
 
+client.disableEmergency();
+
 server.listen(8080);
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
 });
 io.sockets.on('connection', function (socket) {
   socket.on('mykeypress', function (data) {
-    if (data === 32) {
+    if (data === ) {
         // Space: stop all activities and simply hover
         client.stop();
     } else if (data === 37) {
         // Arrow left: rotate counterclockwise
-        client.counterClockwise();
+        client.counterClockwise(0.5);
     } else if (data === 39) {
         // Arrow right: rotate clockwise
-        client.clockwise();
-    } else if (data === 27) {
+        client.clockwise(0.5);
+    } else if (data === 76 || data === 108) {
         // Escape: land the drone
         client.land();
     } else if (data === 13) {
@@ -29,17 +31,11 @@ io.sockets.on('connection', function (socket) {
         client.takeoff();
     } else if (data === 70 || data === 102) {
         client.animateLeds('snakeGreenRed', 3, 10);
+    } else {
+        client.animateLeds('blinkOrange', 3, 5);
     }
   });
 });
 ~                      
 
 //client.config('general:navdata_demo', 'FALSE');
-
-function blink(client) {
-    client.animateLeds('snakeGreenRed', 3, 10);
-}
-
-function setRotateSpeed(client, speed){
-    client.clockwise(speed);
-}
