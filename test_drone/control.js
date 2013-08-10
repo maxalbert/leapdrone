95dca6ff1075f9fbac4af564ede0d8361dee0bc5
Maximilian Albert <maximilian.albert@gmail.com>
2013-08-10 15:10:00 +0100

    Make the drone react to keyboard events from the browser via a socket.

--- test_drone/control.js
+++ test_drone/control.js
@@ -2,6 +2,37 @@ var arDrone = require('ar-drone');
 var client  = arDrone.createClient();
 var events = require('events');
 var eventEmitter = new events.EventEmitter();
+var app = require('express')()
+var server = require('http').createServer(app)
+var io = require('socket.io').listen(server);
+
+server.listen(8080);
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+io.sockets.on('connection', function (socket) {
+  socket.on('mykeypress', function (data) {
+    if (data === 32) {
+        // Space: stop all activities and simply hover
+        client.stop();
+    } else if (data === 37) {
+        // Arrow left: rotate counterclockwise
+        client.counterClockwise();
+    } else if (data === 39) {
+        // Arrow right: rotate clockwise
+        client.clockwise();
+    } else if (data === 27) {
+        // Escape: land the drone
+        client.land();
+    } else if (data === 13) {
+        // Return: take off
+        client.takeoff();
+    } else if (data === 70 || data === 102) {
+        client.animateLeds('snakeGreenRed', 3, 10);
+    }
+  });
+});
+~                      
 
 //client.config('general:navdata_demo', 'FALSE');
 
@@ -12,18 +43,3 @@ function blink(client) {
 function setRotateSpeed(client, speed){
     client.clockwise(speed);
 }
-
-eventEmitter.on('crazyBlink', blink);
-eventEmitter.on('setRotateSpeed', setRotateSpeed);
-
-client.takeoff();
-client
-  .after(5000, function() {
-    eventEmitter.emit('setRotateSpeed', client, 0.5);
-	eventEmitter.emit('crazyBlink', client);
-  })
-  .after(5000, function() {
-    this.stop();
-    this.land();
-  });
-
